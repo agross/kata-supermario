bb1ed7ca2090491d8e6d4e4ca74dba2d08ec6b22
Alexander Groß <agross@therightstuff.de>
2017-10-18 23:43:35 +0200

    Iteration 3

--- src/Mario.Specs/Mario.fs
+++ src/Mario.Specs/Mario.fs
@@ -9,11 +9,11 @@ let iteration1 =
     testCase "starts small" <| fun _ ->
       Expect.equal player.size Small ""
 
-    testCase "dies when hit" <| fun _ ->
-      let subject =
-        player
-        |> hit
-      Expect.equal subject.alive false ""
+    // testCase "dies when hit" <| fun _ ->
+    //   let subject =
+    //     player
+    //     |> hit
+    //   Expect.equal subject.hearts Dead ""
   ]
 
 [<Tests>]
@@ -40,20 +40,24 @@ let iteration2 =
       Expect.equal subject.bag None ""
       Expect.equal subject.size Small ""
   ]
-      let subject = pickupMushroom player
-      Expect.equal subject.bag (Some Mushroom) ""
 
-    testCase "carries maximum of one mushroom" <| fun _ ->
-      let subject =
-        pickupMushroom player
-        |> pickupMushroom
-      Expect.equal subject.bag (Some Mushroom) ""
+[<Tests>]
+let iteration3 =
+  testList "lifes" [
+    testCase "starts with 3 lifes" <| fun _ ->
+      Expect.equal player.hearts (Lifes 3) ""
 
-    testCase "loses mushroom and becomes small when hit" <| fun _ ->
+    testCase "loses 1 life when dying" <| fun _ ->
       let subject =
         player
-        |> pickupMushroom
         |> hit
-      Expect.equal subject.bag None ""
-      Expect.equal subject.size Small ""
+      Expect.equal subject.hearts (Lifes 2) ""
+
+    testCase "dead after losing 3 lifes" <| fun _ ->
+      let subject =
+        player
+        |> hit
+        |> hit
+        |> hit
+      Expect.equal subject.hearts Dead ""
   ]
--- src/Mario/Player.fs
+++ src/Mario/Player.fs
@@ -6,24 +6,36 @@ type Size =
 type Bag =
   | Mushroom
 
+type Hearts =
+  | Dead
+  | Lifes of int
+  with
+  static member (-) (lhs:Hearts, rhs:Hearts) =
+    match (lhs, rhs) with
+    | (Lifes l, Lifes r) when (l - r) > 0 ->
+      Lifes (l - r)
+    | _ -> Dead
+
 type SuperMario =
   {
-    alive : bool
+    hearts : Hearts
     size : Size
     bag : Bag option
   }
 
 let player =
   {
-    alive = true
-    size = Size.Small
+    hearts = Lifes 3
+    size = Small
     bag = None
   }
 
 let hit player =
   match player with
-  | { bag = Some _ } -> { player with bag = None}
-  | _ -> { player with alive = false }
+  | { bag = Some _ } ->
+    { player with bag = None}
+  | _ ->
+    { player with hearts = player.hearts - (Lifes 1) }
 
 let pickupMushroom player =
-  { player with bag = Some Bag.Mushroom }
+  { player with bag = Some Mushroom }
